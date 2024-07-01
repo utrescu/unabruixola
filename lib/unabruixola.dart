839c6fb76807b3fb4967fedfb0105115caf213f0
Xavier Sala <utrescu@gmail.com>
2024-07-01 12:22:21 +0200

    feat: separa classes

---
+++ lib/unabruixola.dart
@@ -0,0 +1,132 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_compass/flutter_compass.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class UnaBruixola extends StatefulWidget {
+  const UnaBruixola({
+    super.key,
+  });
+
+  @override
+  _UnaBruixolaState createState() => _UnaBruixolaState();
+}
+
+class _UnaBruixolaState extends State<UnaBruixola> {
+  bool _hasPermissions = false;
+  // CompassEvent? _lastRead;
+  // DateTime? _lastReadAt;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fetchPermissionStatus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text('UNA BRUIXolA'),
+        ),
+        body: Builder(builder: (context) {
+          if (_hasPermissions) {
+            return Column(
+              children: <Widget>[
+                // _buildManualReader(),
+                Expanded(child: _buildCompass()),
+              ],
+            );
+          } else {
+            return _buildPermissionSheet();
+          }
+        }),
+      ),
+    );
+  }
+
+
+  Widget _buildCompass() {
+    return StreamBuilder<CompassEvent>(
+      stream: FlutterCompass.events,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error llegint la capsalera: ${snapshot.error}');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        double? direction = snapshot.data!.heading;
+
+        // if direction is null, then device does not support this sensor
+        // show error message
+        if (direction == null) {
+          return const Center(
+            child: Text("El dispositiu no té sensors !"),
+          );
+      }
+        return Material(
+          shape: const CircleBorder(),
+          clipBehavior: Clip.antiAlias,
+          elevation: 4.0,
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            alignment: Alignment.center,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+            child: Transform.rotate(
+              angle: (direction * (math.pi / 180) * -1),
+              child: Image.asset('assets/compass.jpg'),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildPermissionSheet() {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const Text('Cal el permís de localització'),
+          ElevatedButton(
+            child: const Text('Demana permisos'),
+            onPressed: () {
+              Permission.locationWhenInUse.request().then((ignored) {
+                _fetchPermissionStatus();
+              });
+            },
+          ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            child: const Text('Obrir la configuració'),
+            onPressed: () {
+              openAppSettings().then((opened) {
+                //
+              });
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  void _fetchPermissionStatus() {
+    Permission.locationWhenInUse.status.then((status) {
+      if (mounted) {
+        setState(() => _hasPermissions = status == PermissionStatus.granted);
+      }
+    });
+  }
+}
